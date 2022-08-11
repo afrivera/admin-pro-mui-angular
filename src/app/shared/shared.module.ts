80557c826e251531cc37df130973d7a5ff843115
afrivera <andresrfelip@hotmail.com>
2022-08-10 21:14:26 -0500

    learning about class of tailwind

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from '../material/material.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
+  
+    SidebarComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
   ],
   exports:[
+    SidebarComponent
   ]
 })
 export class SharedModule { }
